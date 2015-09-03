226804b62bb6423829892e169ca606216587c788
mkdegraw <mkdegraw@gmail.com>
2015-09-03 14:17:37 -0500

    Added a vibration to alarm as well as making sure the alarm goes off at max volume, even if system alarm volume has been turned down

--- app/src/main/java/com/example/michaeldegraw/babyalert/DebugActivity.java
+++ app/src/main/java/com/example/michaeldegraw/babyalert/DebugActivity.java
@@ -7,6 +7,7 @@ import android.media.MediaPlayer;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,6 +21,7 @@ public class DebugActivity extends Activity {
 
     final Context context = this;
     private MediaPlayer player;
+    private int currentVolume;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,22 +64,38 @@ public class DebugActivity extends Activity {
     public void alarmStopButtonClickHandler(View view) {
         Button alarm = (Button) findViewById(R.id.btnAlarm);
         Button alarmStop = (Button) findViewById(R.id.btnAlarmStop);
+        final AudioManager audio = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+        final Vibrator vib = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
 
         Toast.makeText(this, "Stopping alarm...", Toast.LENGTH_SHORT).show();
         player.stop();
+        player.release();
+        vib.cancel();
+        audio.setStreamVolume(AudioManager.STREAM_ALARM, currentVolume, 0);
         alarmStop.setVisibility(View.INVISIBLE);
         alarm.setVisibility(View.VISIBLE);
     }
 
+
+    // THIS FUNCTIION NEEDS TO PLAY THE ALARM, NO MATTER WHAT
+    //  TEST SCENARIOS INCLUDE: IF THE USER IS ON THE PHONE,
+    //  IF THE USER IS LISTENING TO MUSIC, IF THE ALARM VOLUME
+    //  HAS BEEN SET TO ZERO, OR LOW, IF THE USER HAS HEADPHONES
+    //  ON
     private void play(Context context, Uri alert) {
         player = new MediaPlayer();
+        long[] times = {0, 100, 100, 100, 500};
         try {
             player.setDataSource(context, alert);
             final AudioManager audio = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-            if (audio.getStreamVolume(AudioManager.STREAM_ALARM) != 0) {
-                player.setAudioStreamType(AudioManager.STREAM_ALARM);
-                player.prepare();
-                player.start();
+            final Vibrator vib = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+            currentVolume = audio.getStreamVolume(AudioManager.STREAM_ALARM);
+            audio.setStreamVolume(AudioManager.STREAM_ALARM, audio.getStreamMaxVolume(AudioManager.STREAM_ALARM), 0);
+            player.setAudioStreamType(AudioManager.STREAM_ALARM);
+            player.prepare();
+            player.start();
+            if (vib.hasVibrator()) {
+                vib.vibrate(times, 0);
             }
         } catch (IOException e) {
             // this may be the worst error message I've ever seen...
